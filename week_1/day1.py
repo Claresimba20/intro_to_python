44f76050e3d76c5ee4c3a89e7e4ada7ff2c3eada
Clare <awuorsimba@gmail.com>
2025-07-01 22:01:09 +0300

    first commit

---
+++ week_1/day1.py
@@ -0,0 +1,16 @@
+name="clare"
+print(name)
+
+current_age=25
+new_age=current_age+10
+print(current_age)
+print(new_age)
+
+name=input("what is your name?")
+print("hello"+name)
+
+PI=3.142
+radius=input("Enter the radius:")
+radius=float(radius)
+area=(PI*radius**2)
+print("The area of the circle is",area)
